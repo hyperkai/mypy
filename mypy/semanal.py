0d9f833b0594112b4ae3533ce183529dc72a30d4
Kai (Kazuya Ito) <kazya.ito.dream@gmail.com>
2025-12-25 13:27:39 +0900

    Refactor argument validation for typevarlike_type

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -4822,7 +4822,13 @@ class SemanticAnalyzer(
         if len(call.args) < 1:
             self.fail(f"Too few arguments for {typevarlike_type}()", context)
             return False
-        if not isinstance(call.args[0], StrExpr) or call.arg_kinds[0] != ARG_POS:
+        if call.arg_kinds[0] == ARG_NAMED:
+            if call.arg_names[0] != 'name':
+                self.fail(f"{typevarlike_type}() expects 'name' keyword as first argument", context)
+            elif not isinstance(call.args[0], StrExpr):
+                self.fail(f"{typevarlike_type}() expects a string literal as first argument", context)            
+            return False
+        elif call.arg_kinds[0] == ARG_POS and not isinstance(call.args[0], StrExpr):
             self.fail(f"{typevarlike_type}() expects a string literal as first argument", context)
             return False
         elif call.args[0].value != name:
